e14eb98c7ad610b97f0adee8ac124cfeabc527c7
BrysonW <37174771+BrysonW24@users.noreply.github.com>
2022-11-01 18:54:16 +1100

    Update Review.py

--- 5-Functions/Review.py
+++ 5-Functions/Review.py
@@ -1,1 +1,15 @@
-TBC
+# Functions Review
+# Bryson Walter 
+# 01-11-22
+
+# https://www.codecademy.com/courses/learn-python-3/lessons/intro-to-functions/exercises/functions-review
+
+# Review -- To recap, we explored:
+    # What problems arise in our programs without functions
+    # What functions are and how to write them
+    # How whitespace plays an important role in how a program will execute our code and more importantly distinguish function code from non-function code
+    # How to pass input values into our functions using parameters and arguments
+    # The difference between built-in and user-defined functions and some common built-in functions python offers
+    # How we can reuse function output with the return keyword, as well as multiple returns.
+    # Where variables can be accessed in our programs that use functions
+    # Let’s practice putting all these concepts together!
